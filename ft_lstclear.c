c2b399efab507d279e6aafe4fc469d8ab9ce36b2
YuriASN <yuriasn@hotmail.com>
2022-11-15 21:06:43 +0000

    Libft V1, need to run testers and check atoi.

---
+++ ft_lstclear.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysantos- <ysantos-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/27 16:44:07 by ysantos-          #+#    #+#             */
+/*   Updated: 2022/03/28 22:11:50 by ysantos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/* Deletes and frees the given node and every
+successor of that node, using the function ’del’ and free.
+Finally, the pointer to the list must be set to NULL. */
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	if (!*lst)
+		return ;
+	while (*lst)
+	{
+		ft_lstdelone(*lst, del);
+		*lst = (*lst)->next;
+	}
+	lst = NULL;
+}
